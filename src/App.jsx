699780b6a12ba599caae943ef2e2ee19a46e4533
jestincybrosys <141900224+jestincybrosys@users.noreply.github.com>
2024-01-07 21:54:18 +0530

    signup routs

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import Top from './Components/Top';
-import ExpenceCalc from './Components/ExpenceCalc';
-import NewExpence from './Components/NewExpence';
-import Login from './Login';
-import Signup from './Signup'; // Import the Signup component directly
-import { auth } from './firebase';
+import React, { useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import Top from "./Components/Top";
+import ExpenceCalc from "./Components/ExpenceCalc";
+import NewExpence from "./Components/NewExpence";
+import Login from "./Login";
+import Signup from "./Signup";
+import { auth } from "./firebase";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -13,7 +18,7 @@ function App() {
 
   const addExpense = (newExpense) => {
     // Your logic to handle the new expense
-    console.log('New Expense:', newExpense);
+    console.log("New Expense:", newExpense);
   };
 
   useEffect(() => {
@@ -31,14 +36,19 @@ function App() {
 
   // Display a loading indicator while checking authentication state
   if (isCheckingAuth) {
-    return <div className='root_div_loading'><p className='loading_p'>Loading...</p></div>;
+    return (
+      <div className="root_div_loading">
+        <p className="loading_p">Loading...</p>
+      </div>
+    );
   }
 
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} /> {/* Add this line for the signup page */}
+        <Route path="/signup" element={<Signup />} />{" "}
+        {/* Add this line for the signup page */}
         <Route
           path="/home"
           element={
@@ -55,7 +65,7 @@ function App() {
         />
         <Route
           path="/"
-          element={<Navigate to={isLoggedIn ? '/home' : '/login'} />}
+          element={<Navigate to={isLoggedIn ? "/home" : "/login"} />}
         />
       </Routes>
     </Router>
